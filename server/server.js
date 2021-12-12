787818fc9c3189e5551cc85a1cf7661dcc740e32
Achelia Selim <achelia.s.s@gmail.com>
2021-12-13 00:39:28 +0100

    / page added

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~Missions~~~~~~~~~~~~~~~~~~~~~~~~~~
-app.get('/missions', (req, res) => {
+app.get('/', (req, res) => {
    res.send('Welcome to Englingo Missions Service');
 });
 

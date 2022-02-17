d3f1186f103729cfce2d052b2b354fae89aa3bef
Achelia Selim <achelia.s.s@gmail.com>
2022-02-17 15:26:45 +0100

    no cors test debugging

--- server/server.js
+++ server/server.js
@@ -11,18 +11,17 @@ const datamuse = require('datamuse');
 const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-    const corsWhitelist = [
-        'https://webrtc-englingo.herokuapp.com',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000',
-        'https://englingo.herokuapp.com',
-        'https://englingo.herokuapp.com/rooms'
-    ];
+    // const corsWhitelist = [
+
+    //     'https://englingo.herokuapp.com',
+    //     'https://englingo.herokuapp.com',
+    //     'https://englingo.herokuapp.com/rooms'
+    // ];
     logger.info(`Printing the req.headers.origin : ${req.headers.origin}` );
-    if (corsWhitelist.indexOf(req.headers.origin) != -1) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-    }
-    // res.header('Access-Control-Allow-Origin', '*');
+    // if (corsWhitelist.indexOf(req.headers.origin) != -1) {
+    //     res.header('Access-Control-Allow-Origin', req.headers.origin);
+    // }
+    // res.header('Access-Control-Allow-Origin', 'https://englingo.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Cookie, Set-Cookie, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD');

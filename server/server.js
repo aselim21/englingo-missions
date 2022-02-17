466c69d30c21579c0328a44f639fb9043feccb83
Achelia Selim <achelia.s.s@gmail.com>
2022-02-17 18:43:17 +0100

    added self als cors and back wie vorher

--- server/server.js
+++ server/server.js
@@ -28,20 +28,21 @@ const PORT = process.env.PORT || 3000;
 // });
 app.use((req, res, next) => {
     const corsWhitelist = [
-        'https://webrtc-englingo.herokuapp.com',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000',
-        'https://englingo.herokuapp.com'
+      'https://webrtc-englingo.herokuapp.com',
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'https://englingo.herokuapp.com',
+      'https://englingo-missions.herokuapp.com'
     ];
     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
     }
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Cookie, Set-Cookie, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Cookie, Set-Cookie, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS, HEAD');
     next();
-});
+  });
 //~~~~~~~~~~~~~~~~~~~~~~~~~~Missions~~~~~~~~~~~~~~~~~~~~~~~~~~
 app.get('/', (req, res) => {
     res.send('Welcome to Englingo Missions Service');
